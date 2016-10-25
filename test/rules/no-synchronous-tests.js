ced00d66459409478d8081e29fdf95a95f120908
Daniel Lundin <d-dog@d-dog.se>
2016-10-25 09:19:23 +0200

    Fixed coverage

--- test/rules/no-synchronous-tests.js
+++ test/rules/no-synchronous-tests.js
@@ -10,77 +10,27 @@ ruleTester.run('no-synchronous-tests', rules['no-synchronous-tests'], {
         'it("");',
         'it("", function() { })',
         'it("", function() { aFunctionCall(); })',
+        'it("", function() { expect(true).to.be.ok(); })',
         'it("", function() { return promise(); })',
         'it("", function() { return promise().then(function() { expected() }) })',
-        'it("", function(done) { getData(function() { done(); })',
-        'it("", () => promise())'
+        'it("", function(done) { getData(function() { done(); }) })',
+        'ignoredFunction(function() { })',
+        'var x = 1;',
+        'var foo = function() { }',
+        {
+            code: 'it("", () => promise() )',
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: 'it("", () => promise )',
+            parserOptions: { ecmaVersion: 6 }
+        }
+
     ],
     invalid: [
         {
             code: 'it("", function() { getData(function(data) { expect(expected).to.be.present() }); })',
-            errors: [ { message: 'Unexpected synchronous test.' }]
+            errors: [ { message: 'Unexpected synchronous test.' } ]
         }
     ]
-    // valid: [
-    //     'it();',
-    //     'it("");',
-    //     'it("", function () { return promise(); });',
-    //     'it("", function () { return promise(); });',
-    //     'it("", function () { var promise = myFn(); return promise; });',
-    //     'var someFn = function(){ }; it("", someFn);',
-    //     'it("", function (done) { });',
-    //     'it("", function (done) { done(); });',
-    //     'it("", function (callback) { callback(); });',
-    //     'it("", function (done) { if (a) { done(); } });',
-    //     'it("", function (done) { function foo() { done(); } });',
-    //     'it("", function (done) { setTimeout(done, 300); });',
-    //     'it("", function (done) { done(new Error("foo")); });',
-    //     'it("", function (done) { promise.then(done).catch(done); });',
-    //     'it.only("", function (done) { done(); });',
-    //     'test("", function (done) { done(); });',
-    //     'test.only("", function (done) { done(); });',
-    //     'before(function (done) { done(); });',
-    //     'after(function (done) { done(); });',
-    //     'beforeEach(function (done) { done(); });',
-    //     'afterEach(function (done) { done(); });',
-    //     'ignoredFunction(function () { });',
-    //     'var foo = function () { };',
-    //     {
-    //         code: 'it("", (done) => { done(); });',
-    //         parserOptions: { ecmaVersion: 6 }
-    //     },
-    //     {
-    //         code: 'it("", () => { return promise(); });',
-    //         parserOptions: { ecmaVersion: 6 }
-    //     },
-    //     {
-    //         code: 'it("", () => promise() );',
-    //         parserOptions: { ecmaVersion: 6 }
-    //     },
-    //     {
-    //         code: 'it("", () => promise.then() );',
-    //         parserOptions: { ecmaVersion: 6 }
-    //     }
-    // ],
-
-    // invalid: [
-    //     {
-    //         code: 'it("", function () { callback(function() { expect() }); });',
-    //         errors: [ { message: 'Unexpected synchronous test.', column: 8, line: 1 } ]
-    //     },
-    //     {
-    //         code: 'it("", () => "not-a-promise" );',
-    //         parserOptions: { ecmaVersion: 6 },
-    //         errors: [ { message: 'Unexpected synchronous test.', column: 8, line: 1 } ]
-    //     },
-    //     {
-    //         code: 'specify("", function () {});',
-    //         errors: [ { message: 'Unexpected synchronous test.', column: 13, line: 1 } ]
-    //     },
-    //     {
-    //         code: 'specify.only("", function () {});',
-    //         errors: [ { message: 'Unexpected synchronous test.', column: 18, line: 1 } ]
-    //     }
-
-    // ]
 });
